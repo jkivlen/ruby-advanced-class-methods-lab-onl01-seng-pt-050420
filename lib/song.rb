42229af64709212f8f64490561b821e37cb12c50
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-06-07 14:55:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,8 +41,9 @@ end
     @@all.sort_by{|s| s.name}
 end
 
-  def self.new_from_filename
+  def self.new_from_filename(name)
     song = self.new
-    song.name = 
-    
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    song
 end
